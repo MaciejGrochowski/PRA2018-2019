9710a7aa04c7f3929975c3d744e0ace2fb3519f4
Maciej <Maciej>
2018-10-08 13:12:42 +0200

    Lesson 1; Hello World

--- src/main/java/pracownia/introduction/Main.java
+++ src/main/java/pracownia/introduction/Main.java
@@ -1,8 +1,16 @@
 package pracownia.introduction;
 
-public class Main {
+import org.apache.log4j.Logger;
+
+public class
+Main {
+
+    static Logger log = Logger.getLogger("Frytki");
 
     public static void main(String [ ] args) {
-        System.out.println("Hello World");
+        System.out.println("Jestem frytkami");
+
+        log.info("Message");
+
     }
 }
